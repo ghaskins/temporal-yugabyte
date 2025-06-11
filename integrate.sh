7bb5aea1d499b90c50bd344b41508c8e3bbffa12
Greg Haskins <greg@manetu.com>
2025-06-11 15:22:57 -0400

    Checkpoint

--- integrate.sh
+++ integrate.sh
@@ -14,16 +14,16 @@ export INTEGRATION_COMPOSE=docker/docker-compose/integrate.yml
 export BINDIR=./target
 
 init_yb() {
-    until temporal-cassandra-tool --ep "${YUGABYTE_SEEDS}" validate-health; do
+    until $BINDIR/temporal-cassandra-tool --ep "${YUGABYTE_SEEDS}" validate-health; do
         echo 'Waiting for Yugabyte to start up.'
         sleep 1
     done
     echo 'Yugabyte started.'
 
     SCHEMA_DIR=./schema/yugabyte/temporal/versioned
-    temporal-cassandra-tool --ep "${YUGABYTE_SEEDS}" create -k "${YUGABYTE_KEYSPACE}" --rf "1"
-    temporal-cassandra-tool --ep "${YUGABYTE_SEEDS}" -k "${YUGABYTE_KEYSPACE}" setup-schema -v 0.0
-    temporal-cassandra-tool --ep "${YUGABYTE_SEEDS}" -k "${YUGABYTE_KEYSPACE}" update-schema -d "${SCHEMA_DIR}"
+    $BINDIR/temporal-cassandra-tool --ep "${YUGABYTE_SEEDS}" create -k "${YUGABYTE_KEYSPACE}" --rf "1"
+    $BINDIR/temporal-cassandra-tool --ep "${YUGABYTE_SEEDS}" -k "${YUGABYTE_KEYSPACE}" setup-schema -v 0.0
+    $BINDIR/temporal-cassandra-tool --ep "${YUGABYTE_SEEDS}" -k "${YUGABYTE_KEYSPACE}" update-schema -d "${SCHEMA_DIR}"
 }
 
 init_es() {
